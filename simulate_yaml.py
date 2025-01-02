6194c62bc6fb160c455870de3822b4b30e21603c
tacongnam <tacongnam.nam@gmail.com>
2025-01-03 05:50:23 +0700

    Update simulate_yaml.py

--- simulate_yaml.py
+++ simulate_yaml.py
@@ -5,13 +5,15 @@ import time
 
 from simulator.network import parameter as para
 
-def run_weight(begin: float, end: float, testcase):
+def run_weight(a1, a2, b1, b2, c1, c2, testcase):
     print("Test weight")
-    print(f"begin: {begin}, end: {end}")
+    print(f"Energy - begin: {a1}, end: {a2}")
+    print(f"Connection - begin: {b1}, end: {b2}")
+    print(f"Covering - begin: {c1}, end: {c2}")
 
-    for q_a in np.arange(0.1, 0.9):
-        for q_b in np.arange(0.1, 0.9):
-            for q_c in np.arange(0.1, 0.9):
+    for q_a in np.arange(a1, a2, 1):
+        for q_b in np.arange(b1, b2, 1):
+            for q_c in np.arange(c1, c2, 1):
                 print(f"{q_a}, {q_b}, {q_c}")
                 p = Simulation(f'data/{testcase}')
                 p.makeNetwork(0.5, 0.5)
@@ -49,10 +51,14 @@ def main():
     run_type = sys.argv[1]
     param1 = float(sys.argv[2])
     param2 = float(sys.argv[3])
-    param3 = sys.argv[4]
+    param3 = float(sys.argv[4])
+    param4 = float(sys.argv[5])
+    param5 = float(sys.argv[6])
+    param6 = float(sys.argv[7])
+    param7 = sys.argv[8]
 
     if run_type == 'weight':
-        run_weight(param1, param2, param3)
+        run_weight(param1, param2, param3, param4, param5, param6, param7)
     # elif run_type == 'co-formula':
     #     run_coefficient(param1, param2, param3)
     # elif run_type == 'theta':
